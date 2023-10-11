811fcfb35e2f4adec89ce481d374791d236da786
Arina <Arina@DESKTOP-C8IAQNC>
2023-10-11 15:55:37 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.itog = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(395, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(447, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Узнать вид треугольника";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(395, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(419, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Очистить поля и начать заново";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(181, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(181, 189);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(181, 268);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(178, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(696, 45);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите стороны треугольника";
+            // 
+            // itog
+            // 
+            this.itog.AutoSize = true;
+            this.itog.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.itog.Location = new System.Drawing.Point(389, 179);
+            this.itog.Name = "itog";
+            this.itog.Size = new System.Drawing.Size(0, 32);
+            this.itog.TabIndex = 9;
+            this.itog.Click += new System.EventHandler(this.itog_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(287, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 36);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "см";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(287, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 36);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "см";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(287, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 36);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "см";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1038, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.itog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label itog;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        string a;
+        string b;
+        string c;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            itog.ForeColor = Color.White;
+            a = textBox1.Text;
+            b = textBox2.Text;
+            c = textBox3.Text;
+            if ((a == b) && (b == c) && ((a == "0") || (a == "")))
+            {
+                itog.Text = "Введите все значения";
+            }
+            else
+            {
+                if ((b == "0") || (a == "0") ||  (c == "0") || (b == "") || (a == "") || (c == ""))
+                    {
+                    itog.Text = "Введите все значения";
+                    if ((Convert.ToDouble(a) == 0) || (Convert.ToDouble(b) == 0) || (Convert.ToDouble(c) == 0));
+                    }
+                else
+                {
+                    if ((Convert.ToDouble(a) + Convert.ToDouble(b) <= Convert.ToDouble(c)) || (Convert.ToDouble(c) + Convert.ToDouble(b) <= Convert.ToDouble(b)) || (Convert.ToDouble(a) + Convert.ToDouble(c) <= Convert.ToDouble(b)))
+                    {
+                        itog.Text = "Такого треугольника не существует";
+                    }
+                    else
+                    {
+                        if (a == b && b == c)
+                        {
+                            itog.Text = "Треугольник равносторонний";
+                        }
+                        if (a == c && b != c && a != "" && b != "" && c != "")
+                        {
+                            itog.Text = "Треугольник равнобедренный";
+                        }
+                        if (a == b && b != c && a != "" && b != "" && c != "")
+                        {
+                            itog.Text = "Треугольник равнобедренный";
+                        }
+                        if (b == c && a != c && a != "" && b != "" && c != "")
+                        {
+                            itog.Text = "Треугольник равнобедренный";
+                        }
+                        if (a != b && b != c && a != "" && b != "" && c != "")
+                        {
+                            itog.Text = "Треугольник разносторонний";
+                        }
+                    }
+                }
+            }
+                
+               
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 8)
+            {
+                return;
+            }
+            char number = e.KeyChar;
+
+            if (!Char.IsDigit(number))
+            { 
+                e.Handled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            itog.Text = " ";
+
+        }
+
+        private void itog_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
